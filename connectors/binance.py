c74476b6bbc438b0b3c3d45dfb5657eb5fa92437
HP <vaibhav.singh_cs18@gla.ac.in>
2021-12-11 12:20:24 +0530

    import logging and requests and define get_contracts using object

--- connectors/binance.py
+++ connectors/binance.py
@@ -1,1 +1,12 @@
-print("hello binance")
+import logging
+import requests
+
+logger = logging.getLogger()
+
+def get_contracts():
+
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+    contracts = []
+
+
+
