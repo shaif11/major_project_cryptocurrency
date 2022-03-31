90e959e66277f96d115a54bc83a3fb023dee2167
HP <vaibhav.singh_cs18@gla.ac.in>
2022-03-31 11:02:28 +0530

    introducing changes

--- connectors/binance.py
+++ connectors/binance.py
@@ -25,36 +25,7 @@ class BinanceFutureClient:
 
         logger.info("Binance Future client Succesfully initialized")
 
-    def make_request(self, method, endpoint, date):
-        if method == "GET":
-           response =  request.get(self.base_url + endpoint, params=data)
-        else:
-            raise ValueError()
 
-        if response.status_code == 200:
-            return response.json()
-        else:
-            logger.error("error while making %s request to %s: %s (error code %s) method, endpoint, response.json(), response.status_code")
-            return None
-
-
-    def get_contacts(self):
-        exchange_info = self.make_request("GET","/fapi/v1/exchangeInfo", None)
-        return
-
-        contracts = dict()
-
-        if exchange_info is not None:
-            for contract in exchange_info['symbols']:
-                contracts[s['pair']] = contract_data
-        return contracts
-
-    def get_historical_candles(self):
-        "https://fapi.binance.com/fapi/v1/exchangeInfo"
-        return
-
-    def get_bid_ask(self):
-        return
 
 
 
