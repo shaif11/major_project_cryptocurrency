6ae2f66078ab647a88fd7a6a23c3c04c4e07ad80
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2022-04-01 11:40:11 +0530

    Execute BinanceFuturesClient

--- venv/main.py
+++ venv/main.py
@@ -20,23 +20,11 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 if __name__ == '__main__':
-    bitmex_contracts = get_contracts()
-    root = tk.Tk();
-    root.configure(bg = "gray12")
-    i  = 0
-    j = 0
 
-    calibri_font = ("Calibri",11,"normal")
+   binance = BinanceFuturesClient(True)
+   print(binance.get_contracts())
 
-
-    for contract in bitmex_contracts:
-        label_widget = tk.Label(root,text =  contract, bg ='gray12', fg ='SteelBlue1',width = 13,font = calibri_font)
-        label_widget.grid(row= i , column = j,sticky = 'ew')
-        if i == 4:
-            j+= 1
-            i =0
-        else:
-            i+=1
-    root.mainloop()
+   root = tk.Tk()
+   root.mainloop()
 
     # jcjdjcvnhfjvfj
