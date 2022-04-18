72e3317db1d9028611682ce337bdfc179c1c359d
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2022-04-18 20:18:12 +0530

    intialize class candle and function

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ class Balance:
         self.margin_balance = float(info['marginBalance'])
         self.wallet_balance = float(info['walletBalance'])
         self.unrealized_pnl = float(info['unrealizedProfit'])
+
+
+class Candle:
+    def __init__(self, candle_info):
+        self.timestamp = candle_info[0]
+        self.open = float(candle_info[1])
+        self.high = float(candle_info[2])
+        self.low = float(candle_info[3])
+        self.close = float(candle_info[4])
+        self.volume = float(candle_info[5])
+
