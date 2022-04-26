d3236449829cd9c7a1cae548e728ff71f4196d8f
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2022-04-26 12:24:32 +0530

    update main file

--- venv/main.py
+++ venv/main.py
@@ -2,10 +2,10 @@ import tkinter as tk
 import logging
 
 from connectors.binance_futures import BinanceFuturesClient
+from connectors.bitmex import BitmexClient
 
 
 logger = logging.getLogger()
-
 
 logger.setLevel(logging.INFO)
 
@@ -13,18 +13,19 @@ stream_handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.INFO)
+
 file_handler = logging.FileHandler('info.log')
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
+
+
 if __name__ == '__main__':
 
-   binance = BinanceFuturesClient("42da1a468b48c41f7974333ed77b22b898d81b914d2bfcc09d1f520da376a6d6cb7f9c30475b5410730640836205e3dca232f9f5115409870bd6fe30dd3add53", True)
-   candles = binance.get_historical_candles
+    binance = BinanceFuturesClient("", "", True)
+    bitmex = BitmexClient("uXr1T711wD-", "", True)
 
-   root = tk.Tk()
-   root.mainloop()
-
-    # jcjdjcvnhfjvfj
+    root = tk.Tk()
+    root.mainloop()
